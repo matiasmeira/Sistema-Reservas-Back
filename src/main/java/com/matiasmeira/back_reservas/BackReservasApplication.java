ead11416fc131de7a7a142de6129fb51d695b7bc
matias <matiasezequielmeira@gmail.com>
2026-04-09 12:45:35 -0300

    Spring Initializr

---
+++ src/main/java/com/matiasmeira/back_reservas/BackReservasApplication.java
@@ -0,0 +1,13 @@
+package com.matiasmeira.back_reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackReservasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matiasmeira/back_reservas/BackReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasmeira.back_reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
